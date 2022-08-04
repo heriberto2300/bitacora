4f99c3cf1c9a43f6b9ab4830b627e7ad5da8df5b
Heriberto Alonso <joalonsom@hotmail.com>
2022-08-03 23:58:28 -0500

    API de courses basica terminada

--- server/src/routes/courses.js
+++ server/src/routes/courses.js
@@ -10,16 +10,43 @@ router.get('/courses', async (req, res) => {
    res.send(courses);
 });
 
-router.post('/courses', (req, res) => {
+router.post('/courses', async (req, res) => {
     const name = req.body.name;
     const hours = req.body.hours;
     const des = req.body.des;
     const days = req.body.days;
     const teacher = req.body.teacher;
 
-    databaseManager.insertCourse(name, hours, des, days, teacher);
+    //Datos de prueba
+    const idUser = 1;
 
-    res.send(req.body);
+    const { insertId } = await databaseManager.insertCourse(name, hours, des, days, teacher);
+
+    await databaseManager.insertUserCourse(idUser, insertId);
+
+    //const headings = req.body.headings;
+    const headings = [
+        {
+            name: 'tareas',
+            valor: '25',
+        },
+        {
+            name: 'exposicion',
+            valor: '25',
+        },
+        {
+            name: 'examenes',
+            valor: '50',
+        }
+    ];
+    headings.map(async (heading) => {
+        let query = await databaseManager.insertHeading(heading.name, heading.valor);
+        let idHeading = query.insertId;
+        await databaseManager.insertCourseHeading(insertId, idHeading);
+    });
+
+    res.send("ok");
+    
 });
 
 module.exports = router;
