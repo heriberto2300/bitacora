cc6b7c04be3e8fa69166eba29285a15c0021947c
Heriberto Alonso <joalonsom@hotmail.com>
2022-08-17 13:41:18 -0500

    Formulario basico de login hecho

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-import Formulario  from './components/Formulario';
+import {
+  BrowserRouter as Router,
+  Route, Routes 
+} from 'react-router-dom';
+
 import './App.css';
+import Root from './pages/Root';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <Formulario/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Root/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+    </Router>
   );
 }
 
