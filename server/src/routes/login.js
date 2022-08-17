eb9712137c8253c20a457e3b74fe4154f7848fff
Heriberto Alonso <joalonsom@hotmail.com>
2022-08-17 13:42:35 -0500

    API login modificado, falta corregir

--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -8,24 +8,22 @@ router.use(JSON);
 router.post('/login', async (req, res) => {
     const name = req.body.name;
     const password = req.body.password;
-    const table = req.body.table;
+    const table = 'Usuarios';
     
     const user = [...await databaseManager.searchByName(name, table)];
-    const passUser = user[0].Password;
-
-    /*if(name === undefined || password === undefined || table === undefined || passUser === undefined) {
-        res.send("SOME UNDEFINED ELEMENT");
-        return;
-    }*/
     
-    if(user.length !== 0 && (await bcryptjs.compare(password, passUser))) {
-        res.send("Se ha encontrado al usuario");
+    //mejorar validacion, ahorita es provisional
+    
+    if(user.length !== 0) {
+        const passUser = user[0].Password;
+        if(await bcryptjs.compare(password, passUser)) {
+            res.send(true);
+        } else {
+            res.send(false);
+        }
     }else{
-        res.send("No se ha encontrado al usuario");
+        res.send(false);
     }
-    //res.send(user);
 });
-
-
 
 module.exports = router;
