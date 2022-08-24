6fce30b39424091ef00f45bebf6d3b9de3498a88
Heriberto Alonso <joalonsom@hotmail.com>
2022-08-23 19:48:40 -0500

    Mejora de la api home.js, dado un usuario, ya se regresa las tareas que tiene asignadas

--- server/src/routes/home.js
+++ server/src/routes/home.js
@@ -4,10 +4,24 @@ const databaseManager = require('../lib/dataBase');
 
 router.use(JSON);
 
-router.get('/home', async (req, res) => {
-    const toDos = await databaseManager.getTableData("Tareas");
-    const status = getPercentage(toDos);
-    res.json({percentage: status, toDos});
+router.post('/home', async (req, res) => {
+    const queryIdUser = `SELECT * FROM Usuarios WHERE Nombre='${req.body.user}'`; 
+    const userData = await databaseManager.query(queryIdUser);
+    const { ID_Usuario } = userData[0];
+    
+    const query = `SELECT * FROM Usuario_Tarea WHERE ID_Usuario=${ID_Usuario}`;
+    const tableData = await databaseManager.query(query);
+
+    let taskList = [];
+
+    for (const data of tableData) {
+        const query = `SELECT * FROM Tareas WHERE ID_Tarea='${data.ID_Tarea}'`;
+        const task = await databaseManager.query(query);
+        taskList.push(task[0]);
+    }  
+
+    tableData.length === 0 ? res.json({empty: true, percentage: 0, tasks: taskList}) : 
+        res.json({empty: false, percentage: getPercentage(taskList), tasks: taskList});
 });
 
 const getPercentage = (toDos) => {
@@ -20,6 +34,5 @@ const getPercentage = (toDos) => {
 
     return (completes * 100) / totalTasks;
 }
-
 
 module.exports = router;
