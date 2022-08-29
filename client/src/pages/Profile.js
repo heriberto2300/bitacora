d7d1991880b79b6c8ace1872d4a5ca9c5484eec8
Heriberto Alonso <joalonsom@hotmail.com>
2022-08-29 16:39:39 -0500

    Componente para pagina perfil creado con estilo

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import NavBar from '../components/NavBar';
+import '../stylesheets/page.css';
+
+function Profile() {
+    return(
+        <div className="page profilePage">
+            <NavBar/>
+        </div>
+    );
+}
+
+export default Profile;
