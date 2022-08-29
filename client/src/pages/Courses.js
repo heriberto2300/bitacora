7e59e44f53ae13a02fa6cb8feea6a8c378b64b21
Heriberto Alonso <joalonsom@hotmail.com>
2022-08-29 16:38:56 -0500

    Componente para pagina materias creado con estilo

---
+++ client/src/pages/Courses.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import NavBar from '../components/NavBar';
+import '../stylesheets/page.css';
+
+function Courses() {
+    return(
+        <div className="page coursesPage">
+            <NavBar/>
+        </div>
+    );
+}
+
+export default Courses;
