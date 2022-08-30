95665492081e4041598f4d5440e5997b58ca238c
Heriberto Alonso <joalonsom@hotmail.com>
2022-08-29 22:08:33 -0500

    Tarea mejorada, no se puede desplegar un icono diferente n base al status

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,12 +1,21 @@
 import React from "react";
-
+import { BsFillCheckCircleFill as Check} from "react-icons/bs";
+import { BsCaretDownFill as DownArrow } from "react-icons/bs";
+import { BsClockFill as Pending } from "react-icons/bs";
 import '../stylesheets/task.css';
 
-function Task({ name, des }) {
+function Task({ name, status, date }) {
   return(
     <div className="taskContainer">
-      <div className="taskTitle">{name}</div> 
-      <div className="taskDescription">{des}</div> 
+      <div className="info">
+        <div className="taskTitle"><h2>{name}</h2></div>
+        <div className="taskStatus"><p>{`Status: ${status}`}</p></div>
+        <div className="taskDate"><p>{`Fecha de Entrega: ${date}`}</p></div>
+      </div>
+      <div className="icons">
+        <p><Check size="70px" color="#fff"/></p>
+        <p className="displayButton"><DownArrow size="20px" color="#fff"/></p>
+      </div> 
     </div>
   );    
 }
