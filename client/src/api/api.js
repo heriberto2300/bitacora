366360ba6390f7b76c8c1d2e8640fd281a1e4aad
Heriberto Alonso <joalonsom@hotmail.com>
2022-10-12 12:03:02 -0500

    api cambiada, ya no se maneja por clases

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,42 +1,35 @@
-class api {
-  async post(url, data) {
-    const params = {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data),
-    }
-    const response = await fetch(url, params);
-    
-    return response;
-  };
-
-  async get(url) {
-    const params = {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    }
-    const response = await fetch(url, params);
-    
-    return response;
+const post = async (url, data) => {
+  const params = {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
   }
+  const response = await fetch(url, params);
+  return response;
+};
+
+const get = async (url) => { 
+  const params = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  }
+  const response = await fetch(url, params);
+  return response;
+};
   
-  async getCourses(user) {
-    const data = {user: user,};
-    const url = "http://192.168.100.11:8000/courses"
-    const response = await this.get(url, data); 
-    return response.json();
-  }
-}
+const getCourses = async (user) => {
+  const data = {user: user,};
+  const url = "http://192.168.100.11:8000/courses"
+  const response = await get(url, data); 
+  return response.json();
+};
 
-const API = new api();
-
-module.exports = API;
-
+export { post, get, getCourses };
 
 
