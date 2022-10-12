9d0efaea2ee9ac66566d4b8fce40f1e88efb6572
Heriberto Alonso <joalonsom@hotmail.com>
2022-10-12 10:23:47 -0500

    Inicio de construccion de despliegue de materias

--- client/src/pages/Courses.js
+++ client/src/pages/Courses.js
@@ -1,14 +1,40 @@
 import React from "react";
+import Course from "../components/Course";
 
 import NavBar from '../components/NavBar';
 import '../stylesheets/Page.css';
+import '../stylesheets/Courses.css';
+
+import API from '../api/api';
 
 function Courses() {
-    return(
-        <div className="page courses-page">
-            <NavBar/>
-        </div>
-    );
+  const [courses, setCourses] = React.useState([]);
+
+  const fetchCourses = async () => {
+    const res = await API.getCourses();
+    const { result } = res;
+    console.log(result);
+    setCourses(result);
+  };
+
+  React.useEffect(() => {
+    fetchCourses();
+  }, []);
+
+  return(
+    <div className="page courses-page">
+      <NavBar title="Materias"/>
+      <div className="courses-container">
+        {courses.map(course => 
+          <Course key={course.ID_Materia}
+            id={course.ID_Materia}
+            name={course.Nombre}
+            description={course.Descripcion}          
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Courses;
